eead30b57f56462bc2d64e099c73fe3787412e44
aniekmarkus <a.markus@erasmusmc.nl>
2023-05-26 13:14:48 +0200

    Update run scripts

--- Readme.md
+++ Readme.md
@@ -1,42 +1,39 @@
-## Install :
-
-TODO: To install the core environment dependencies of OpenXAI, use `pip` by cloning this repo into your local environment:
-
-```bash
-pip install -e . 
-```
-
+This repository contains the code for the evaluation framework and experiments presented in:
+***Understanding the Size of the Feature Importance Disagreement Problem in Real-World Data***
 
 ## Replicate experiments:
 
-Run pipeline for specific settings:
+Run experiments for specific settings:
+* [data] in ['iris', 'vote', 'compas', 'german', 'copdmortality', 'heartfailurestroke'] # last two not public
+* [model] in ['logistic', 'nn', 'xgboost']
+* [fi_method] in ['permutation_auc', 'permutation_mse', 'permutation_ba', 'loco_auc', 'loco_mse', 'loco_ba', 'sage_marginal', 'sage_conditional', 'kernelshap']
+* [repeats] in [1, 2, 3, etc.]
 
 ```
-   python python/experiments.py [data] [model] [fi_method] [repeats] [rerun]
+   python python/experiments.py [data] [model] [fi_method] [repeats]
 ```
 
 Run all experiments:
 ```
 sh run.sh
-# TODO: add flag --no-output
 ```
 
 ## Check results:
 
 Aggregate output for specific settings:
 ```
-python output.py [data]
+python output.py [data] [model]
 ```
 
-Aggregate all experiments:
+Aggregate all output:
 ```
 sh run.sh
-# TODO: add flag --show-output
 ```
 
 Checkout plots and tables in:
 ```
 /results/
+/plots/
 ```
 
 Interactive python DASH app:
--- run.sh
+++ run.sh
@@ -2,81 +2,45 @@
 
 ## RUN EXPERIMENTS
 
-folder="output_2023-05-07"
+folder="output_2023-ICML"
 
-# format:
-# python python/experiments.py [data] [model] [fi_method] [repeats]
+# TESTING
+# python python/experiments.py iris logistic permutation_auc 1 $folder --clean
 
-# [data] in ['iris']
-# [model] in ['linear', 'logistic']
-# [fi_method] in ['permutation_auc']
-# [repeats] in [1, 2, 3, etc.]
-
-# Code testing
-# python python/experiments.py compas logistic sage_conditional 1
-
-# Main experiments
-python python/experiments.py compas logistic permutation_ba 1 $folder --clean
-
-for d in vote compas
-do
-  for m in logistic nn xgboost
+# MAIN EXPERIMENTS
+# Baseline
+ for d in vote compas german copdmortality heartfailurestroke
+ do
+  for m in logistic nn
     do
-      python python/experiments.py $d $m permutation_auc 1 $folder
-
-      for f in permutation_mse sage_marginal sage_conditional loco_auc loco_mse kernelshap
+      python python/experiments.py $d $m permutation_auc 5 $folder
+      for f in permutation_mse loco_auc loco_mse loco_ba kernelshap sage_marginal sage_conditional
       do
           echo "Run for dataset $d model $m and feature importance method $f"
-          python python/experiments.py $d $m $f 1 $folder --use-model
+          python python/experiments.py $d $m $f 5 $folder --use-model
+      done
+    done
+ done
+
+# Modify data
+for d in compas german
+do
+  for m in logistic nn
+    do
+      python python/experiments.py $d $m permutation_auc 5 $folder --modify-data
+      for f in permutation_mse loco_auc loco_mse loco_ba kernelshap sage_marginal sage_conditional
+      do
+          echo "Run for dataset $d model $m and feature importance method $f"
+          python python/experiments.py $d $m $f 5 $folder --modify-data --use-model
       done
     done
 done
 
-# GERMAN
-#python python/experiments.py german logistic permutation_auc 1 --clean
-#python python/experiments.py german logistic permutation_mse 1 --use-model
-#python python/experiments.py german logistic permutation_ba 1 --use-model
-#python python/experiments.py german logistic loco_auc 1 --use-model
-#python python/experiments.py german logistic loco_mse 1 --use-model
-#python python/experiments.py german logistic loco_ba 1 --use-model
-
-#python python/experiments.py german nn permutation_auc 1
-#python python/experiments.py german nn permutation_mse 1 --use-model
-#python python/experiments.py german nn permutation_ba 1 --use-model
-#python python/experiments.py german nn loco_auc 1 --use-model
-#python python/experiments.py german nn loco_mse 1 --use-model
-#python python/experiments.py german nn loco_ba 1 --use-model
-
-#python python/experiments.py german xgboost permutation_auc 1
-#python python/experiments.py german xgboost permutation_mse 1 --use-model
-#python python/experiments.py german xgboost permutation_ba 1 --use-model
-#python python/experiments.py german xgboost loco_auc 1 --use-model
-#python python/experiments.py german xgboost loco_mse 1 --use-model
-#python python/experiments.py german xgboost loco_ba 1 --use-model
-
-#python python/experiments.py german logistic kernelshap 1 --use-model
-#python python/experiments.py german logistic sage_marginal 1 --use-model
-
-# TODO
-#python python/experiments.py german nn kernelshap 1 --use-model
-#python python/experiments.py german nn sage_marginal 1 --use-model
-
-#python python/experiments.py german xgboost kernelshap 1 --use-model
-#python python/experiments.py german xgboost sage_marginal 1 --use-model
-
 ### AGGREGATE OUTPUT
-
-# format:
-# python python/output.py [data] [model]
-
-#python python/output.py german logistic
-
-#for d in iris
-#do
-#  for m in logistic nn xgboost
-#    do
-#      python python/output.py $d $m
-#    done
-#done
-
-### VISUALIZE OUTPUT
+for d in vote compas german copdmortality heartfailurestroke
+do
+  for m in logistic nn
+    do
+      python python/output.py $d $m $folder
+    done
+done
